e0deb94a29ac80a466f11be183d47b4deb487e52
reshma7598 <reshmarajanr136@gmail.com>
2024-08-08 19:49:37 +0530

    first commit

---
+++ javascript/arraymethod.js
@@ -0,0 +1,19 @@
+// function coviddata(n){
+// desenting=find(n).sort(n<15000)
+// console.log(coviddata(desenting));
+// }
+// coviddata(n)=positivecases;
+// let positivecases=[[1,'ekm',34000],
+// [2,'idk',15000],
+// [3,'thr',24000],
+// [4,'pkd',12000],
+// [5,'koz',44000]]
+
+
+
+
+function coviddata(){
+    console.log(thr);
+    
+}
+let thr=[positive='24000',death='4000',current='33000',firststdose='24000',seconddose='2500']
